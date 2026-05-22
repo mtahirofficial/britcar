101ab23eea7d3b129a4e90e6c9b996566762d87e
Muhammad Tahir <30388791+mtahirofficial@users.noreply.github.com>
2026-05-22 20:09:04 +0500

    Update ordersController.js

--- controller/ordersController.js
+++ controller/ordersController.js
@@ -33,7 +33,7 @@ const ordersController = {
       method: "POST",
       url: `https://${domain}/admin/api/2025-07/graphql.json`,
       data: JSON.stringify({
-        query: `{productVariants(first: 10, query: "barcode:*${searchQuery}* title:*${searchQuery}* sku:*${searchQuery}*") {
+        query: `{productVariants(first: 10, query: "sku:*${searchQuery}*") {
           edges {
             node {
               barcode
